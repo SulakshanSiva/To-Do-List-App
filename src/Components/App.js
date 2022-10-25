ab2676e9899310934a38e3e444776a6d85825157
Sulakshan <sulakshansivak@gmail.com>
2022-10-24 20:08:13 -0400

    setup json database to save notes

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,19 +2,39 @@ import '../Styles/App.css';
 import AddNote from './AddNote';
 import Navbar from './Navbar';
 import DisplayNotes from './DisplayNotes';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
     const [toDoList, setToDoList] = useState({list: []});
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/list")
+        .then((response) => response.json())
+        .then((toDoList) => setToDoList({list: toDoList}))
+    }, []);
 
     //callback function
     const addNoteToList = (note) => {
         let currentList = toDoList["list"];
-        currentList.push(note);
-        setToDoList({list : currentList});
-        console.log(toDoList);
+
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(note),
+
+        };
+        fetch("http://localhost:3000/list", requestOptions)
+        .then((response) => response.json())
+        .then((toDoList) =>{
+            currentList.push(note);
+            setToDoList({list : currentList});
+        });
+        
     }
+
 
     return (
     <div className="App">
